3f610296566e04f7e57a04667273b351a342b281
Corey Donohoe <atmos@atmos.org>
2016-12-17 22:32:14 -0800

    don't need these

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,4 +4,3 @@ Rails.application.config.session_store :redis_store, servers: {
                                                      },
                                                      expires_in: 5.minutes,
                                                      key: "_epicmix_session"
-Rollbar.configuration.scrub_fields |= [ "_epicmix_session", "session_id" ]
