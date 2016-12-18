86b2c553ffaca50b31abe2cf9d9146a920230b26
Corey Donohoe <atmos@atmos.org>
2016-12-17 22:28:37 -0800

    whoops

--- config/initializers/rollbar.rb
+++
@@ -1,8 +0,0 @@
-Rollbar.configure do |config|
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
-  if Rails.env.test? || Rails.env.development?
-    config.enabled = false
-  end
-  config.scrub_fields  |= Rollbar::Blanket.fields
-  config.scrub_headers |= Rollbar::Blanket.headers
-end
