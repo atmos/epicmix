0b46b40eb35592e7494867f5de3d99401062bea4
Corey Donohoe <atmos@atmos.org>
2016-12-19 17:49:15 -0800

    more gems and work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "root#index"
   post "/commands", to: "commands#create"
+
+  get  "/auth/failure",                to: "sessions#destroy"
+  get  "/auth/slack/callback",         to: "sessions#create_slack"
+  get  "/auth/slack_install/callback", to: "sessions#install_slack"
 end
