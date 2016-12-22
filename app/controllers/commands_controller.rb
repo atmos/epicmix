47f7463797b002af8bac4a0cb793db982d77fb9e
Corey Donohoe <atmos@atmos.org>
2016-12-21 21:45:12 -0800

    try authenticating

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -4,13 +4,25 @@ class CommandsController < ApplicationController
 
   def create
     if slack_token_valid?
-      render json: { text: ":soon:", response_type: "in_channel" }, status: 201
+      if current_user && current_user.user.token
+        render json: {
+          text: current_user.user.current_user.vertical_feet,
+          response_type: "in_channel"
+        }, status: 201
+      else
+        render json: { text: ":soon:", response_type: "in_channel" }, status: 201
+      end
     else
       render json: {}, status: 403
     end
   end
 
   private
+
+  def current_user
+    @current_user ||= User.find_by(slack_user_id: params[:user_id],
+                                   slack_team_id: params[:team_id])
+  end
 
   def slack_token
     ENV["SLACK_SLASH_COMMAND_TOKEN"]
