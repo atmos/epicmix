fb73a407815d2ab8ba939da6947c043c782b23fc
Corey Donohoe <atmos@atmos.org>
2016-12-25 14:30:18 -0800

    fixup migrations hopefully

---
+++ db/migrate/20161225222920_add_users.rb
@@ -0,0 +1,12 @@
+class AddUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+      t.string   "slack_user_id",   null: false
+      t.string   "slack_user_name", null: false
+      t.string   "slack_team_id",   null: false
+      t.string   "epicmix_email"
+      t.string   "enc_epicmix_password"
+      t.timestamps
+    end
+  end
+end
