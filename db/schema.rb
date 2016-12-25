6edd294de1166858a186b51c8c12851c7a8f4d8c
Corey Donohoe <atmos@atmos.org>
2016-12-25 14:27:10 -0800

    :cry:

--- db/schema.rb
+++ db/schema.rb
@@ -17,13 +17,12 @@ ActiveRecord::Schema.define(version: 20161221074829) do
   enable_extension "uuid-ossp"
 
   create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "slack_user_id",        null: false
-    t.string   "slack_user_name",      null: false
-    t.string   "slack_team_id",        null: false
+    t.string   "slack_user_id",   null: false
+    t.string   "slack_user_name", null: false
+    t.string   "slack_team_id",   null: false
     t.string   "epicmix_email"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.string   "enc_epicmix_password"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
