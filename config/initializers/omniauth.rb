33ef6a7759f93aeb54b9f0f83c72789e7e3660f6
Corey Donohoe <atmos@atmos.org>
2016-12-27 11:39:57 -0800

    less scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :slack, ENV["SLACK_OAUTH_ID"], ENV["SLACK_OAUTH_SECRET"], scope: "identity.basic"
-  provider :slack, ENV["SLACK_OAUTH_ID"], ENV["SLACK_OAUTH_SECRET"], scope: "commands,identify,users:read,team:read", name: :slack_install
+  provider :slack, ENV["SLACK_OAUTH_ID"], ENV["SLACK_OAUTH_SECRET"], scope: "commands,identify,team:read", name: :slack_install
 end
 
 OmniAuth.config.logger = Rails.logger
