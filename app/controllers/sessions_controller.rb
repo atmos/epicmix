fec048003b8b765002c73c7844be065f1148fc25
Corey Donohoe <atmos@atmos.org>
2016-12-27 11:29:41 -0800

    try logging more

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   include SessionsHelper
 
   def install_slack
-    Rails.logger.info omniauth: omniauth_info
+    Rails.logger.info omniauth: omniauth_info.to_json
     redirect_to "/auth/slack"
   end
 
