03026985923aff4fa1ef6102aee9ee64170d353a
Corey Donohoe <atmos@atmos.org>
2016-12-28 13:58:03 -0800

    break users and stuff out

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -5,11 +5,15 @@ class Leaderboard
     @users = users
   end
 
+  def user_rows
+    users.map { |u| [u.slack_user_name, u.vertical_feet] }
+  end
+
   def ascii_table
     @ascii_table ||= Terminal::Table.new do |t|
       t << ["Name", "Vertical Feet"]
       t << :separator
-      t.add_row users.map { |u| [u.slack_user_name, u.vertical_feet] }
+      user_rows.each { |u| t.add_row u }
     end
   end
 
